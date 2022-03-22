2dfcf93b6034bf8f3b0c4d7085af2d7678e61e12
Jeong101 <chox623@gmail.com>
2022-03-22 15:56:02 +0900

    20220322

--- src/main/java/allbaro/cf/controller/AllbaroExcelController.java
+++ src/main/java/allbaro/cf/controller/AllbaroExcelController.java
@@ -40,22 +40,25 @@ public class AllbaroExcelController {
 		workbook = new XSSFWorkbook(path + "/" + fileName);
 	}
 	//실제 계산값 작성
-	public void writeDetail(XSSFSheet sheet,XSSFRow curRow,XSSFCell cell,ArrayList<CF_dto> data) {
+	public void writeDetail(XSSFSheet sheet,XSSFRow curRow,XSSFCell cell,ArrayList<CF_dto> data1) {
 		/*
 		 *1. 수집,재활용을 기준으로 data를 2개의 컬렉션으로 구분
 		 *2. 인계서일련번호를 기준으로 날짜가 같으면 재활용항목까지 모두 작성
 		 *2-1. 같은날 재활용하지 않았을 시, 재활용컬렉션에 있는 재활용일자 확인 후 작성
 		 *3. 파일로 출력
 		 * */
+		ArrayList<CF_dto> data = new ArrayList<>();
+		data = getInputData(data1,1);
 		int rowNo = 6;
-		short cellNo = 0;
-		int cnt = 0;
+		
+		//int cnt = 0;
 		Iterator<CF_dto> itr = data.iterator();
 		
 		//while(itr.hasNext()) {
-		curRow = sheet.createRow(rowNo);
-		
-		
+		for(int cnt=0;cnt<data.size();cnt++) {
+		short cellNo=0;
+			
+		curRow = sheet.createRow(rowNo);	
 		//날짜작성
 		cell = curRow.createCell(cellNo++);
 		cell.setCellValue(data.get(cnt).getDate());
@@ -82,27 +85,29 @@ public class AllbaroExcelController {
 		cell.setCellValue("C.F");
 		//소재지
 		cell = curRow.createCell(cellNo++);
-		sheet.addMergedRegion(new CellRangeAddress(6, 6, 8, 9));
+		sheet.addMergedRegion(new CellRangeAddress(rowNo, rowNo, 8, 9));
 		cell.setCellValue("죽산면 걸미로 478-12");
 		//사용용도
-		cell = curRow.createCell(cellNo++);
-		cell.setCellValue(data.get(cnt).getInAmount());
+		cell = curRow.createCell(++cellNo);
+		cell.setCellValue("재활용");
 		//공급량
+		cell = curRow.createCell(++cellNo);
+		cell.setCellValue(data.get(cnt).getOutAmount());
 		
+		rowNo++;
 		
-		cnt ++;
-		//}
+		}
 	}
 
 	public ArrayList<CF_dto> getInputData(ArrayList<CF_dto> all,int flag){
 		//flag로 수집날짜,재활용날짜 data구분 1:수집 0:재활용
 		ArrayList<CF_dto> data = new ArrayList<CF_dto>();//수집,재활용 dto 구별
 		Iterator<CF_dto> itr = data.iterator();
-		int cnt;
+		int cnt=0;
 		switch(flag) {
 		
 		case 1://수집 data
-			cnt=0;
+			
 			while(itr.hasNext()) {
 			if(all.get(cnt).getInAmount()!=0) {
 				data.add(all.get(cnt));
@@ -113,8 +118,8 @@ public class AllbaroExcelController {
 			break;
 			
 		case 2://재활용 data
-			if(all.get(1).getOutAmount()!=0) {
-				data.add(all.get(1));
+			if(all.get(cnt).getOutAmount()!=0) {
+				data.add(all.get(cnt));
 			}
 			break;
 		}
